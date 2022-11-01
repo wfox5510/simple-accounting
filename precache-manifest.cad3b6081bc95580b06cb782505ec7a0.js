548d09e5c09ca275af23360ff570f0b771411605
wfox5510 <wfox5510@google.com>
2022-11-01 14:20:44 +0800

    Updates

---
+++ precache-manifest.cad3b6081bc95580b06cb782505ec7a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "086e44b92703570c19c4",
+    "url": "/simple-accounting/static/css/main.b079521b.chunk.css"
+  },
+  {
+    "revision": "086e44b92703570c19c4",
+    "url": "/simple-accounting/static/js/main.086e44b9.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/simple-accounting/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "f1f1eecfe51ac8019931",
+    "url": "/simple-accounting/static/js/2.f1f1eecf.chunk.js"
+  },
+  {
+    "revision": "390cfd920c4628c07bcf",
+    "url": "/simple-accounting/static/js/runtime~main.390cfd92.js"
+  },
+  {
+    "revision": "fc90fd85176556a85a7fafd51ecb1184",
+    "url": "/simple-accounting/index.html"
+  }
+];
