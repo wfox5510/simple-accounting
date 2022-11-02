167e4f8025b4fc27ee690f9de2f49074e49ef53c
wfox5510 <wfox5510@google.com>
2022-11-02 17:05:05 +0800

    fix button bug / map key error in chart / add README

--- src/BuildChart.js
+++ src/BuildChart.js
@@ -1,30 +1,28 @@
 import React, { useState } from 'react';
 import './BuildChart.css';
-var profitData
+var profitData = []
 function BuildChart({ profitYearData }) {
     const [chartState, setChartState] = useState("nonDisplay")
     function listDataToBar() {
-        profitData = profitYearData.map(function (list, index) {
-            let bar = parseInt((list) / 4000)
-            if (parseInt(list) >= 0) {
+        console.log(profitYearData)
+        for (var i = 0; i < 10; i++) {
+            let bar = parseInt((profitYearData[i]) / 4000)
+            if (parseInt(profitYearData[i]) >= 0) {
                 if (bar > 50) bar = 50
-                return (
-                    <div className="result-bg" data-year={`${2020 + index}`}>
+                profitData[i] =
+                    <div className="result-bg" data-year={`${2020 + i + 1}`}>
                         <div className="result-bar" style={{ height: `${bar}%`, background: '#69f788' }}></div>
                     </div>
-
-                )
             }
-            if (parseInt(list) < 0) {
+            if (parseInt(profitYearData[i]) < 0) {
                 bar *= -1
                 if (bar > 50) bar = 50
-                return (
-                    <div className="result-bg" data-year={`${2020 + index}`}>
+                profitData[i] =
+                    <div className="result-bg" data-year={`${2020 + i + 1}`}>
                         <div className="result-bar2" style={{ height: `${bar}%`, bottom: `${50 - bar}%` }}></div>
                     </div>
-                )
             }
-        })
+        }
     }
 
     function chartBTN() {
